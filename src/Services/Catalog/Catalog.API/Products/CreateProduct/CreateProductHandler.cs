b7de60c063e578801db4a71a31fc95109bc014cb
ahmetkarademirr <ahmet.karademir@portx.net>
2024-07-10 11:29:43 +0300

    add-cqrs-interfaces

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,16 @@
+﻿using BuildingBlocks.CQRS;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+    : ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+
+            throw new NotImplementedException();
+        }
+    }
+}
